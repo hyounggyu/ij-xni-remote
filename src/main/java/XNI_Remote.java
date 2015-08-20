80476d74b91a8bf6dc941e30008f874a918002a0
Hyounggyu Kim <hgkim10@gmail.com>
2015-08-20 23:00:25 +0900

    Add remote image loading

--- src/main/java/XNI_Remote.java
+++ src/main/java/XNI_Remote.java
@@ -1,11 +1,52 @@
 import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
 import ij.plugin.PlugIn;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class XNI_Remote implements PlugIn {
+
+    public ImagePlus getImagePlus() throws IOException {
+        Socket s = new Socket("127.0.0.1", 5051);
+
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        DataInputStream in = new DataInputStream(s.getInputStream());
+
+        out.write(0); // Send hello
+
+        byte dt = in.readByte();
+        int z = in.readInt(); // Shape 0: frame
+        int h = in.readInt(); // Shape 1: height
+        int w = in.readInt(); // Shape 2: width
+
+        ImageStack stack = new ImageStack(w, h);
+        IJ.showProgress(0, z);
+        for (int i = 0; i < z; i++) {
+            float[] data = new float[h*w];
+            for (int j = 0; j < h*w; j++) {
+                data[j] = in.readFloat();
+            }
+            stack.addSlice("", data);
+            IJ.showProgress(i + 1, z);
+        }
+
+        return new ImagePlus("stack", stack);
+    }
+
     @Override
     public void run(String arg) {
-        IJ.showMessage("This would be our plugin!");
+        try {
+            ImagePlus image = getImagePlus();
+            image.show();
+        } catch (IOException e) {
+            IJ.showMessage("Connection Error");
+        }
     }
+
     public static void main(final String... args) {
         new ij.ImageJ();
         new XNI_Remote().run("");
